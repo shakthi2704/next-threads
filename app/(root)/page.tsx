379eb7dc7514d897ba691665462219fe57d3c77c
Satheesh <shakthi2704@gmail.com>
2023-12-15 16:44:59 +0530

    basic_layout

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { UserButton } from "@clerk/nextjs"
+
+const Home = () => {
+  return (
+    <>
+      <h1 className="head-text text-left">Home</h1>
+    </>
+  )
+}
+
+export default Home
